eeb15d02b18e4c1acf55b330c15e7b98c6e683a5
Tensae <tensaeb2017@gmail.com>
2025-02-07 11:25:49 +0300

    complain route modified

--- src/routes/complaint.route.ts
+++ src/routes/complaint.route.ts
@@ -25,18 +25,18 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype.startsWith("image/") ||
-      file.mimetype.startsWith("video/")
-    ) {
-      cb(null, true);
-    } else {
-      cb(
-        new Error("Not an image or video! Please upload only images or videos.")
-      );
-    }
-  },
+  // fileFilter: (req, file, cb) => {
+  //   if (
+  //     file.mimetype.startsWith("image/") ||
+  //     file.mimetype.startsWith("video/")
+  //   ) {
+  //     cb(null, true);
+  //   } else {
+  //     cb(
+  //       new Error("Not an image or video! Please upload only images or videos.")
+  //     );
+  //   }
+  // },
   limits: { fileSize: 300 * 1024 * 1024 },
 });
 
