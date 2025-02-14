405ab3988338f82b801701eb2b308d613ed8c7a1
Tensae <tensaeb2017@gmail.com>
2025-02-14 11:18:27 +0300

    password required changed

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,7 +5,7 @@ const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
     role: {
       type: String,
       enum: [
